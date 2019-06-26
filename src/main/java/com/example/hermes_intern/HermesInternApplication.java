9989cb7d8c0153f73e2f1f2363e981f6d0631786
Oğuzhan Karagöl <oguzhankaragol60@gmail.com>
2019-06-26 11:14:48 +0300

    initial commit

---
+++ src/main/java/com/example/hermes_intern/HermesInternApplication.java
@@ -0,0 +1,13 @@
+package com.example.hermes_intern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HermesInternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HermesInternApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hermes_intern/HermesInternApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.hermes_intern;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HermesInternApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
