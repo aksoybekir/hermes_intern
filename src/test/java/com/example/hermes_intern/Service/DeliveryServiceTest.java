bfeaaf1add7bc455237e993647ba9d95c06b1450
Oğuzhan Karagöl <oguzhankaragol60@gmail.com>
2019-07-08 18:10:04 +0300

    delivery service tests completed

--- src/test/java/com/example/hermes_intern/Service/DeliveryServiceTest.java
+++ src/test/java/com/example/hermes_intern/Service/DeliveryServiceTest.java
@@ -1,7 +1,11 @@
 package com.example.hermes_intern.Service;
 
+import com.example.hermes_intern.domain.Actions;
 import com.example.hermes_intern.domain.Customer;
 import com.example.hermes_intern.domain.Delivery;
+import com.example.hermes_intern.domain.DeliveryStatus;
+import com.example.hermes_intern.model.DeliveryCheckIn;
+import com.example.hermes_intern.model.DeliveryCheckOut;
 import com.example.hermes_intern.repository.ReactiveDeliveryRepository;
 import com.example.hermes_intern.security.JWTUtil;
 import com.example.hermes_intern.service.DeliveryService;
@@ -19,9 +23,15 @@ import reactor.test.StepVerifier;
 
 import javax.imageio.stream.MemoryCacheImageOutputStream;
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.description;
 import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
@@ -38,12 +48,12 @@ public class DeliveryServiceTest {
 
 
     @Before
-    public void setup(){
-        deliveryService = new DeliveryService(reactiveDeliveryRepository,jwtUtil);
+    public void setup() {
+        deliveryService = new DeliveryService(reactiveDeliveryRepository, jwtUtil);
     }
 
     @Test
-    public void getAllTest(){
+    public void getAllTest() {
 
         Delivery delivery = new Delivery();
         delivery.setId("mytestdeliveryid");
@@ -61,7 +71,7 @@ public class DeliveryServiceTest {
     }
 
     @Test
-    public void getCourierMyDeliveriesTest(){
+    public void getCourierMyDeliveriesTest() {
         Principal principal = new Principal() {
             @Override
             public String getName() {
@@ -79,7 +89,7 @@ public class DeliveryServiceTest {
 
         StepVerifier
                 .create(source)
-                .consumeNextWith(response ->{
+                .consumeNextWith(response -> {
                     assertEquals("courierid", response.getCourierid());
                     assertEquals("ON_COURIER", response.getStatus());
                 })
@@ -89,7 +99,7 @@ public class DeliveryServiceTest {
     }
 
     @Test
-    public void getByStatusOnCourierTest(){
+    public void getByStatusOnCourierTest() {
 
         Delivery delivery = new Delivery();
         delivery.setId("mytestdeliveryid");
@@ -100,7 +110,7 @@ public class DeliveryServiceTest {
 
         StepVerifier
                 .create(source)
-                .consumeNextWith(response ->{
+                .consumeNextWith(response -> {
                     assertEquals("ON_COURIER", response.getStatus());
                 })
                 .expectComplete()
@@ -108,7 +118,7 @@ public class DeliveryServiceTest {
     }
 
     @Test
-    public void getByStatusInBranchTest(){
+    public void getByStatusInBranchTest() {
 
         Delivery delivery = new Delivery();
         delivery.setId("mytestdeliveryid");
@@ -119,7 +129,7 @@ public class DeliveryServiceTest {
 
         StepVerifier
                 .create(source)
-                .consumeNextWith(response ->{
+                .consumeNextWith(response -> {
                     assertEquals("IN_BRANCH", response.getStatus());
                 })
                 .expectComplete()
@@ -127,7 +137,7 @@ public class DeliveryServiceTest {
     }
 
     @Test
-    public void getByStatusInWarehouseTest(){
+    public void getByStatusInWarehouseTest() {
 
         Delivery delivery = new Delivery();
         delivery.setId("mytestdeliveryid");
@@ -138,14 +148,14 @@ public class DeliveryServiceTest {
 
         StepVerifier
                 .create(source)
-                .consumeNextWith(response ->{
+                .consumeNextWith(response -> {
                     assertEquals("IN_WAREHOUSE", response.getStatus());
-                })                .expectComplete()
+                }).expectComplete()
                 .verify();
     }
 
     @Test
-    public void getByStatusOnWayWarehouseTest(){
+    public void getByStatusOnWayWarehouseTest() {
 
         Delivery delivery = new Delivery();
         delivery.setId("mytestdeliveryid");
@@ -161,5 +171,107 @@ public class DeliveryServiceTest {
                 })
                 .expectComplete()
                 .verify();
+    }
+
+    @Test
+    public void getDeliveyCountByStatusTodayTest()
+    {
+        List<Delivery> deliveries = new ArrayList<>();
+
+        deliveries.add(new Delivery());
+        deliveries.add(new Delivery());
+        deliveries.add(new Delivery());
+
+        when(reactiveDeliveryRepository.getDeliveryCountByDateCourierRecieved(DeliveryStatus.ON_COURIER.toString(), deliveryService.getStartOfToday(), deliveryService.getStartOfTomarrow())).thenReturn(Flux.fromIterable(deliveries));
+        when(reactiveDeliveryRepository.getDeliveryCountByDateDeliveredToBranch(DeliveryStatus.IN_BRANCH.toString(), deliveryService.getStartOfToday(), deliveryService.getStartOfTomarrow())).thenReturn(Flux.fromIterable(deliveries));
+        when(reactiveDeliveryRepository.getDeliveryCountByDateLeftBranch(DeliveryStatus.ON_WAY_WAREHOUSE.toString(), deliveryService.getStartOfToday(), deliveryService.getStartOfTomarrow())).thenReturn(Flux.fromIterable(deliveries));
+        when(reactiveDeliveryRepository.getDeliveryCountByDateDeliveredToWarehouse(DeliveryStatus.IN_WAREHOUSE.toString(), deliveryService.getStartOfToday(), deliveryService.getStartOfTomarrow())).thenReturn(Flux.fromIterable(deliveries));
+
+        StepVerifier.create(deliveryService.getDeliveyCountByStatusToday(DeliveryStatus.IN_BRANCH.toString())).consumeNextWith(deliveryCount -> {
+            assertThat(deliveryCount).isNotNull();
+            assertThat(deliveryCount.getDeliveryCount()).isEqualTo(3);
+        }).verifyComplete();
+    }
+
+    @Test
+    public void getCourierDeliveriesTodayTest() {
+        Principal principal = () -> "courierid";
+
+        Delivery delivery = new Delivery();
+        delivery.setId("mytestdeliveryid");
+
+
+        when(reactiveDeliveryRepository.getDeliveryCountByDateDeliveredToBranchAndCourierId(DeliveryStatus.IN_BRANCH.toString(), principal.getName(), deliveryService.getStartOfToday(), deliveryService.getStartOfTomarrow())).thenReturn(Flux.just(delivery));
+        when(reactiveDeliveryRepository.getDeliveryCountByDateCourierRecievedAndCourierId(DeliveryStatus.ON_COURIER.toString(), principal.getName(), deliveryService.getStartOfToday(), deliveryService.getStartOfTomarrow())).thenReturn(Flux.just(delivery));
+
+        StepVerifier.create(deliveryService.getCourierDeliveriesToday("courierid", "IN_BRANCH")).consumeNextWith(delivery1 -> {
+            assertThat(delivery1).isNotNull();
+        }).verifyComplete();
+    }
+
+    @Test
+    public void getCourierMyDeliveriesTodayTest() {
+        Principal principal = () -> "courierid";
+
+        Delivery delivery = new Delivery();
+        delivery.setId("mytestdeliveryid");
+
+        when(reactiveDeliveryRepository.getDeliveryCountByDateCourierRecievedAndCourierId(DeliveryStatus.IN_BRANCH.toString(), principal.getName(), deliveryService.getStartOfToday(), deliveryService.getStartOfTomarrow())).thenReturn(Flux.just(delivery));
+
+        StepVerifier.create(deliveryService.getCourierMyDeliveriesToday(principal)).consumeNextWith(delivery1 -> {
+            assertThat(delivery1).isNotNull();
+        }).verifyComplete();
+
+    }
+
+    @Test
+    public void checkInDeliveryBranchTest() {
+        Delivery delivery = new Delivery();
+        delivery.setId("mytestdeliveryid");
+        delivery.setActions(new Actions());
+
+        when(reactiveDeliveryRepository.findById(anyString())).thenReturn(Mono.just(delivery));
+        when(reactiveDeliveryRepository.save(any(Delivery.class))).thenReturn(Mono.just(delivery));
+
+        StepVerifier.create(deliveryService.checkInDeliveryBranch(delivery.getId())).consumeNextWith(deliveryCheckIn ->
+        {
+            assertThat(deliveryCheckIn).isNotNull();
+            assertThat(deliveryCheckIn.getId()).isEqualTo(delivery.getId());
+            assertThat(deliveryCheckIn.isSuccess()).isEqualTo(true);
+        }).verifyComplete();
+    }
+
+    @Test
+    public void checkInDeliveryWarehouseTest() {
+        Delivery delivery = new Delivery();
+        delivery.setId("mytestdeliveryid");
+        delivery.setActions(new Actions());
+
+        when(reactiveDeliveryRepository.findById(anyString())).thenReturn(Mono.just(delivery));
+        when(reactiveDeliveryRepository.save(any(Delivery.class))).thenReturn(Mono.just(delivery));
+
+        StepVerifier.create(deliveryService.checkInDeliveryWarehouse(delivery.getId())).consumeNextWith(deliveryCheckIn ->
+        {
+            assertThat(deliveryCheckIn).isNotNull();
+            assertThat(deliveryCheckIn.getId()).isEqualTo(delivery.getId());
+            assertThat(deliveryCheckIn.isSuccess()).isEqualTo(true);
+        }).verifyComplete();
+    }
+
+    @Test
+    public void checkOutDeliveryTest() {
+        Delivery delivery = new Delivery();
+        delivery.setId("mytestdeliveryid");
+        delivery.setActions(new Actions());
+
+        when(reactiveDeliveryRepository.findById(anyString())).thenReturn(Mono.just(delivery));
+        when(reactiveDeliveryRepository.save(any(Delivery.class))).thenReturn(Mono.just(delivery));
+
+        StepVerifier.create(deliveryService.checkOutDelivery(delivery.getId())).consumeNextWith(deliveryCheckOut ->
+        {
+            assertThat(deliveryCheckOut).isNotNull();
+            assertThat(deliveryCheckOut.getId()).isEqualTo(delivery.getId());
+            assertThat(deliveryCheckOut.isSuccess()).isEqualTo(true);
+        }).verifyComplete();
     }
 }
